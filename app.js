d86cdeb1788ba5d25fa369a760ea3b07895e5158
Jahongir <Jahongir>
2025-08-16 10:55:32 +0500

    sasa

---
+++ app.js
@@ -0,0 +1,142 @@
+const body = document.querySelector(".body");
+body.style.cssText = `
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+`
+
+const container = document.querySelector(".container")
+container.style.cssText = `
+    max-width: 1440px;
+    padding: 15px;
+`
+
+
+const parent = document.querySelector(".parent");
+parent.style.cssText = `
+    width: 920px;
+    height: 500px;
+    display: flex;
+    overflow: hidden;
+    border-radius: 8px;
+    margin-top: 170px;
+    margin-right: 260px;
+    margin-left: 260px;
+`;
+
+const button = document.querySelector(".button");
+button.style.cssText = `
+  padding: 11px 31px;
+    border-radius: 25px;
+    background-color: #F2F2F2;
+    text-decoration: none;
+    color: #E28625;
+    font-family: "Lexend Deca";
+`
+const button2 = document.querySelector(".button2");
+button2.style.cssText = `
+    padding: 11px 31px;
+    border-radius: 25px;
+    background-color: #F2F2F2;
+    text-decoration: none;
+    color: #006971;
+    font-family: "Lexend Deca";
+`
+const button3 = document.querySelector(".button3");
+button3.style.cssText = `
+    padding: 11px 31px;
+    border-radius: 25px;
+    background-color: #F2F2F2;
+    text-decoration: none;
+    color: #004140;
+    font-family: "Lexend Deca";
+`
+const child1 = document.querySelector(".child1");
+child1.style.cssText = `
+    width: 307px;
+    height: 500px;
+    background-color: #E28625;
+    padding: 48px;
+`
+const child2 = document.querySelector(".child2");
+child2.style.cssText = `
+    width: 307px;
+    height: 500px;
+    background-color: #006971;
+    padding: 48px;
+`
+const child3 = document.querySelector(".child3");
+child3.style.cssText = `
+    width: 307px;
+    height: 500px;
+    background-color: #004140;
+    padding: 48px;
+`
+const child1h1 = document.querySelector(".child1h1");
+child1h1.style.cssText = `
+    margin-top: 35px;
+    margin-bottom: 25px;
+    font-family: "Big Shoulders";
+    color: #F2F2F2;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 100%;
+`
+const child2h2 = document.querySelector(".child2h2");
+child2h2.style.cssText = `
+    margin-top: 35px;
+    margin-bottom: 25px;
+    font-family: "Big Shoulders";
+    color: #F2F2F2;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 100%;
+    `
+const child3h3 = document.querySelector(".child3h3");
+child3h3.style.cssText = `
+    margin-top: 35px;
+    margin-bottom: 25px;
+    font-family: "Big Shoulders";
+    color: #F2F2F2;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 100%;
+`
+const child1p1 = document.querySelector(".card1p1");
+child1p1.style.cssText = `
+    width: 200px;
+    
+    color: #f8e1c8;
+    /* opacity: 75%; */
+    margin-bottom: 83px;
+    font-family: "Lexend Deca";
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 25px;
+    letter-spacing: 0px;
+`
+const child2p2 = document.querySelector(".child2p2");
+child2p2.style.cssText = `
+    width: 200px;
+    color: #bfd9db;
+    margin-bottom: 83px;
+    font-family: "Lexend Deca";
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 25px;
+    letter-spacing: 0px;
+`
+const child3p3 = document.querySelector(".child3p3");
+child3p3.style.cssText = `
+    width: 200px;
+    color: #bfcfcf;
+    margin-bottom: 83px;
+    font-family: "Lexend Deca";
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 25px;
+    letter-spacing: 0px;
+`
+
+
+
